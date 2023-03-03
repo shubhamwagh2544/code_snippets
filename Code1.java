2a52c5644ae3368303b62a1853fb67b1e2453a55
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-03-04 00:01:22 +0530

    more with stream

--- Code1.java
+++ Code1.java
@@ -12,13 +12,22 @@ public class Code1 {
         map.put("D", 4000);
         map.put("E", 5000);
 
+        System.out.println("----------------map : foreach-----------------");
+
         map.entrySet().stream().forEach(entry -> {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         });
 
-        String key = map.entrySet().stream().sorted(Comparator.comparing(entry -> -entry.getValue())).collect(Collectors.toList())
-                .get(0).getKey();
+        System.out.println("------------map : filter---------------");
+
+        String key = map.entrySet().stream().filter(entry -> entry.getValue() == 4000).findFirst().get().getKey();
         System.out.println(key);
+
+        System.out.println("---------------map : sorted-----------------");
+
+        String key1 = map.entrySet().stream().sorted(Comparator.comparing(entry -> -entry.getValue())).collect(Collectors.toList())
+                .get(0).getKey();
+        System.out.println(key1);
 
         List<String> list = new ArrayList<>();
         list.add("A");
@@ -27,10 +36,20 @@ public class Code1 {
         list.add("ABCD");
         list.add("ABCDE");
 
+        System.out.println("---------------list : sorted------------------");
+
         String s1 = list.stream().sorted(Comparator.comparing(s -> -s.length())).collect(Collectors.toList()).get(0);
         System.out.println(s1);
 
+        System.out.println("----------------list : filter--------------");
+
         String ab = list.stream().filter(s -> s.startsWith("AB")).findFirst().get();
         System.out.println(ab);
+
+        System.out.println("---------------list : map----------------------");
+
+        list.stream().map(s -> s+"adding").collect(Collectors.toList()).forEach(
+                s -> System.out.println(s)
+        );
     }
 }
